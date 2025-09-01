aa956abffe36861c9c89d87fd6b9649fec526ceb
Manoj Vadigireddygari <manojvadigireddygari@Manojs-MacBook-Air.local>
2025-09-01 13:06:53 +0530

    Added Security Config at API Gateway to permit some endpoints with out Authentication

---
+++ APIGateway/src/main/java/com/example/APIGateway/Configuration/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.APIGateway.Configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/api/microservice3/**").permitAll()
+                        .pathMatchers("/api/manoj/**").permitAll()
+                        .anyExchange().authenticated()           // secure everything else
+                );
+
+
+        return http.build();
+    }
+}
--- Microservice-1/src/main/java/com/example/manoj/Controller/employeeController.java
+++ Microservice-1/src/main/java/com/example/manoj/Controller/employeeController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/manoj")
 public class employeeController {
 
     @Autowired
